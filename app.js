ef98deb6ee3b04ed59f3ca88fe72d59c2e975779
BenchiBench <38825061+BenchiBench@users.noreply.github.com>
2026-02-05 20:27:47 +0800

    更新 app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let BUILD_DIR = 'dist'
 let ENTRY_FILE = './node.txt'
 let str = readSync(ENTRY_FILE)
 
-// Map all the item include `ssr://` and serialize those items
-let result = str.split('\n\n').filter(item => item.includes('ssr://')).join('\r\n')
+// Map all the item include `ss://` and serialize those items
+let result = str.split('\n\n').filter(item => item.includes('ss://')).join('\r\n')
 
 writeSync(path.resolve(__dirname, BUILD_DIR, 'index.html'), base64(result))
